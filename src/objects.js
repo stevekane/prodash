bddff29e33fd9d2b6e32f077048ec6731e819526
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-09-23 18:15:27 -0500

    added definitions for functions on objects useing transducers

--- src/objects.js
+++ src/objects.js
@@ -1,16 +1,50 @@
+var fns         = require("./functions")
 var transducers = require("./transducers")
 var mapping     = transducers.mapping
 var filtering   = transducers.filtering
-var obj         = {}
+var curry       = fns.curry
+var object      = {}
 
 var keys = Object.keys
 
-//TODO: implement w/ transducers
-var map = function (fn, o) {
-  return o 
+var cons = function (host, obj) {
+  var index = -1
+  var ks    = keys(obj)
+  var len   = ks.length
+  var key
+
+  while (++index < len) {
+    key       = ks[index]
+    host[key] = obj[key]
+  }
+  return host
 }
 
-obj.keys = keys
-obj.map  = map
+var reduce = function (fn, accum, obj) {
+  var index = -1
+  var ks    = keys(obj)
+  var len   = ks.length
+  var key
 
-module.exports = obj
+  while (++index < len) {
+    key   = ks[index]
+    accum = fn(accum, obj[key])  
+  }
+  return accum
+}
+
+var map = function (fn, obj) {
+  return reduce(mapping(fn, cons), [], obj)
+}
+
+var filter = curry(function (predFn, ar) {
+  return reduce(filtering(predFn, cons), [], obj)
+})
+
+object.keys   = keys
+object.cons   = cons
+object.map    = map
+object.reduce = reduce
+object.filter = filter
+
+module.exports = object
