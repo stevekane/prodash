91a43df358d875b18fb9f1ad6e81d0bb889adc4e
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-09-24 14:54:22 -0500

    added tree to the main module exports and revved version

--- prodash.js
+++ prodash.js
@@ -3,6 +3,7 @@ var prodash = {
   transducers: require("./src/transducers"),
   array:       require("./src/array"),
   object:      require("./src/object"),
+  tree:        requre("./src/tree")
 }
 
 module.exports = prodash
