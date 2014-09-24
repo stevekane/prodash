e0714fd762375a846fffcfc968b305b5c650f817
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-09-24 14:17:46 -0500

    updated source and tests for graphs.  removed map function for the moment and tested transducers functionality

--- src/graph.js
+++ src/graph.js
@@ -1,7 +1,4 @@
-var transducers = require("./transducers")
 var fns         = require("./functions")
-var mapping     = transducers.mapping
-var filtering   = transducers.filtering
 var curry       = fns.curry
 var g           = {}
 
@@ -18,37 +15,27 @@ var cons = function (node, childNode) {
   return node
 }
 
-var reduce = function reduce (fn, accum, node) {
+var reduce = curry(function reduce (fn, accum, node) {
   accum = fn(accum, node.value)
 
   for (var i = 0; i < node.children.length; ++i) {
     reduce(fn, accum, node.children[i]) 
   }
   return accum
-}
+})
 
-var map = function (fn, node) {
-  var r = Node(fn(node.value))
-
-  for (var i = 0; i < node.children.length; ++i) {
-    cons(r, map(fn, node.children[i]))
-  }
-  return r
-}
-
-var forEach = function (fn, node) {
+var forEach = curry(function (fn, node) {
   fn(node.value)  
 
   for (var i = 0; i < node.children.length; ++i) {
     forEach(fn, node.children[i])
   }
   return node
-}
+})
 
 g.Node    = Node
 g.cons    = cons
 g.reduce  = reduce
-g.map     = map
 g.forEach = forEach
 
 module.exports = g
--- tests/graph.js
+++ tests/graph.js
@@ -1,12 +1,15 @@
-var test    = require('tape')
-var mod     = require('../src/graph.js')
-
-var Node    = mod.Node
-var cons    = mod.cons
-var reduce  = mod.reduce
-var map     = mod.map
-var forEach = mod.forEach
-var push    = function (ar, x) {
+var test      = require('tape')
+var mod       = require('../src/graph.js')
+var trans     = require("../src/transducers.js")
+var fns       = require("../src/functions")
+var mapping   = trans.mapping
+var filtering = trans.filtering
+var compose   = fns.compose
+var Node      = mod.Node
+var cons      = mod.cons
+var reduce    = mod.reduce
+var forEach   = mod.forEach
+var push      = function (ar, x) {
   ar.push(x) 
   return ar
 }
@@ -44,17 +47,6 @@ test('reduce', function (t) {
   t.same(r[2], {id: 2})
 })
 
-test('map', function (t) {
-  var grandChildren = [Node({id: 22})]
-  var childNodes    = [Node({id: 1}, grandChildren), Node({id: 2})]
-  var root          = Node({id: 99}, childNodes)
-  var newGraph      = map(makeRocket, root)
-
-  //TODO: IMPLEMENT ACTUAL TESTS.  THIS APPEARS TO BE WORKING
-  t.plan(1)
-  t.true(true)
-})
-
 test('forEach', function (t) {
   var grandChildren = [Node({id: 22})]
   var childNodes    = [Node({id: 1}, grandChildren), Node({id: 2})]
@@ -64,4 +56,19 @@ test('forEach', function (t) {
   forEach(makeRocket, root)
   t.plan(1)
   t.true(true)
+})
+
+test('test that we can compose functions using transducers', function (t) {
+  var grandChildren = [Node({id: 22})]
+  var childNodes    = [Node({id: 1}, grandChildren), Node({id: 2})]
+  var root          = Node({id: 99}, childNodes)
+  var removeLow     = filtering(function (x) { return x.id > 15 })
+  var incId         = mapping(function (x) { x.id++; return x })
+  var processTree   = compose([removeLow, incId])
+  var newNodes      = reduce(processTree(push), [], root)
+
+  t.plan(3)
+  t.true(newNodes.length === 2)
+  t.same(newNodes[0].id, 100)
+  t.same(newNodes[1].id, 23)
 })
