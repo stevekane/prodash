693e310c4d2183351df207dbf20ce07864be915e
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-09-25 02:18:54 -0500

    refactored the graph file to use a deep reduction mapping and removed the concept of a value inside a node.  The children are simply added as a key to the provided hash

--- src/graph.js
+++ src/graph.js
@@ -1,12 +1,12 @@
 var fns         = require("./functions")
+var array       = require("./array")
+var consA       = array.cons
 var curry       = fns.curry
 var g           = {}
 
 var Node = function (hash, nodes) {
-  return {
-    value:    hash,
-    children: nodes || [] 
-  }
+  hash.children = nodes || []
+  return hash
 }
 
 var cons = function (node, childNode) {
@@ -15,7 +15,7 @@ var cons = function (node, childNode) {
 }
 
 var reduce = curry(function reduce (fn, accum, node) {
-  accum = fn(accum, node.value)
+  accum = fn(accum, node)
 
   for (var i = 0; i < node.children.length; ++i) {
     reduce(fn, accum, node.children[i]) 
@@ -23,17 +23,8 @@ var reduce = curry(function reduce (fn, accum, node) {
   return accum
 })
 
-var reduceDeep = curry(function reduceDeep (fn, accum, node) {
-  accum = fn(accum, node)
-
-  for (var i = 0; i < node.children.length; ++i) {
-    reduceDeep(fn, accum, node.children[i]) 
-  }
-  return accum
-})
-
 var forEach = curry(function (fn, node) {
-  fn(node.value)  
+  fn(node)  
 
   for (var i = 0; i < node.children.length; ++i) {
     forEach(fn, node.children[i])
@@ -41,10 +32,19 @@ var forEach = curry(function (fn, node) {
   return node
 })
 
+/*
+ * Iterate over all nodes in the tree pushing them onto an array
+ * Note.  This is simply a special case of the reduce function
+ * where all outputs are pushed onto an array
+ */
+var flatten = function (redFn, graph) {
+  return reduce(redFn(consA), [], graph)
+}
+
 g.Node       = Node
 g.cons       = cons
 g.reduce     = reduce
-g.reduceDeep = reduceDeep
 g.forEach    = forEach
+g.flatten    = flatten
 
 module.exports = g
--- tests/graph.js
+++ tests/graph.js
@@ -9,13 +9,14 @@ var Node      = mod.Node
 var cons      = mod.cons
 var reduce    = mod.reduce
 var forEach   = mod.forEach
+var flatten   = mod.flatten
 var push      = function (ar, x) {
   ar.push(x) 
   return ar
 }
-var makeRocket = function (val) {
-  val.type = "rocket"
-  return val
+var makeRocket = function (n) {
+  n.type = "rocket"
+  return n
 }
 
 test('Node', function (t) {
@@ -23,9 +24,9 @@ test('Node', function (t) {
   var root       = Node({id: 99}, childNodes)
 
   t.plan(3)
-  t.same(root.value.id, 99)
-  t.same(root.children[0].value.id, 1)
-  t.same(root.children[1].value.id, 2)
+  t.same(root.id, 99)
+  t.same(root.children[0].id, 1)
+  t.same(root.children[1].id, 2)
 })
 
 test('cons', function (t) {
@@ -33,7 +34,7 @@ test('cons', function (t) {
 
   cons(root, Node({id: 2}))
   t.plan(1)
-  t.same(root.children[0].value.id, 2) 
+  t.same(root.children[0].id, 2) 
 })
 
 test('reduce', function (t) {
@@ -42,9 +43,9 @@ test('reduce', function (t) {
   var r          = reduce(push, [], root)
 
   t.plan(3)
-  t.same(r[0], {id: 99})
-  t.same(r[1], {id: 1})
-  t.same(r[2], {id: 2})
+  t.same(r[0].id, 99)
+  t.same(r[1].id, 1)
+  t.same(r[2].id, 2)
 })
 
 test('forEach', function (t) {
@@ -52,23 +53,28 @@ test('forEach', function (t) {
   var childNodes    = [Node({id: 1}, grandChildren), Node({id: 2})]
   var root          = Node({id: 99}, childNodes)
 
-  //TODO: IMPLEMENT ACTUAL TESTS.  THIS APPEARS TO BE WORKING
   forEach(makeRocket, root)
-  t.plan(1)
-  t.true(true)
+  t.plan(4)
+  t.same(root.type, "rocket")
+  t.same(root.children[0].type, "rocket")
+  t.same(root.children[1].type, "rocket")
+  t.same(root.children[0].children[0].type, "rocket")
 })
 
-test('test that we can compose functions using transducers', function (t) {
+test('flatten', function (t) {
   var grandChildren = [Node({id: 22})]
   var childNodes    = [Node({id: 1}, grandChildren), Node({id: 2})]
   var root          = Node({id: 99}, childNodes)
   var removeLow     = filtering(function (x) { return x.id > 15 })
   var incId         = mapping(function (x) { x.id++; return x })
+  var identity      = mapping(function (x) { return x })
   var processTree   = compose([removeLow, incId])
-  var newNodes      = reduce(processTree(push), [], root)
+  var someNodes     = flatten(processTree, root)
+  var allNodes      = flatten(identity, root)
 
-  t.plan(3)
-  t.true(newNodes.length === 2)
-  t.same(newNodes[0].id, 100)
-  t.same(newNodes[1].id, 23)
+  t.plan(4)
+  t.true(someNodes.length === 2)
+  t.same(someNodes[0].id, 100)
+  t.same(someNodes[1].id, 23)
+  t.true(allNodes.length === 4)
 })
