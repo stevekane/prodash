be068a6e34251aedc3d8b2595cfac711e7608002
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-09-28 22:46:30 -0500

    removed obsolete graph.js from prodash export

--- prodash.js
+++ prodash.js
@@ -2,8 +2,7 @@ var prodash = {
   functions:   require("./src/functions"),
   transducers: require("./src/transducers"),
   array:       require("./src/array"),
-  object:      require("./src/object"),
-  graph:       require("./src/graph")
+  object:      require("./src/object")
 }
 
 module.exports = prodash
